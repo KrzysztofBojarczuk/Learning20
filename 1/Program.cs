614920b0c4b0d87b5cc68a0acb35e86954f39a75
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-03-10 12:58:03 +0100

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pierwszaLiczba = 10, drugaLiczba = 20, i = 0, j = 0, k = 0;
+
+            Console.WriteLine("pierwszaLiczba: " + pierwszaLiczba);
+            Console.WriteLine("drugaLiczba: " + drugaLiczba);
+            Console.WriteLine("zmienna i: " + i);
+            Console.WriteLine("zmienna j: " + j);
+            Console.WriteLine("zmienna k: " + k);
+
+
+
+            
+
+
+
+        }
+    }
+}
